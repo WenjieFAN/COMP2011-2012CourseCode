953756a7c402c6438305a29c8bd7b843b1e418d0
Wenjie FAN <48483586+WenjieFAN@users.noreply.github.com>
2019-03-14 21:52:36 +0100

    Add files via upload

---
+++ NumberCard.h
@@ -0,0 +1,18 @@
+/*
+ * NumberCard.h
+ *
+ *  Created on: Jan 29, 2018
+ *      Author: kevinw
+ */
+
+#ifndef NUMBERCARD_H_
+#define NUMBERCARD_H_
+
+#include <iostream>
+#include "Card.h"
+
+class NumberCard: public Card{
+
+};
+
+#endif /* NUMBERCARD_H_ */
